fcb53ac0b57614b070c077f0db32a96fb430e32d
gigabit101 <thegigabit101@gmail.com>
2017-01-03 13:46:43 +0000

    Very basic mod

--- src/main/java/RebornStorage/RebornStorage.java
+++ src/main/java/RebornStorage/RebornStorage.java
@@ -1,8 +1,23 @@
 package RebornStorage;
 
+import RebornStorage.init.ModBlocks;
+import RebornStorage.init.ModItems;
+import RebornStorage.init.ModRecipes;
+import RebornStorage.lib.ModInfo;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
 /**
  * Created by Gigabit101 on 03/01/2017.
  */
+@Mod(name = ModInfo.MOD_NAME, modid = ModInfo.MOD_ID, version = ModInfo.MOD_VERSION)
 public class RebornStorage
 {
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        ModItems.init();
+        ModBlocks.init();
+        ModRecipes.init();
+    }
 }
---
+++ src/main/java/RebornStorage/init/ModBlocks.java
@@ -0,0 +1,12 @@
+package RebornStorage.init;
+
+/**
+ * Created by Gigabit101 on 03/01/2017.
+ */
+public class ModBlocks
+{
+    public static void init()
+    {
+
+    }
+}
