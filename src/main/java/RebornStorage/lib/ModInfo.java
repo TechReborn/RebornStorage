f1c5f06d57303f98abb35cc737fb09b4e0ba58b2
gigabit101 <thegigabit101@gmail.com>
2017-01-06 21:58:14 +0000

    Fixed dependencies issue

--- src/main/java/RebornStorage/lib/ModInfo.java
+++ src/main/java/RebornStorage/lib/ModInfo.java
@@ -8,7 +8,7 @@ public class ModInfo
     public static final String MOD_ID = "rebornstorage";
     public static final String MOD_NAME = "RebornStorage";
     public static final String MOD_VERSION = "1.0.0";
-    public static final String MOD_DEPENDENCUIES ="required-after:reborncore";
-    public static final String COMMON_PROXY_LOC = "required-after:forge;required-after:reborncore;after:refinedstorage";
+    public static final String MOD_DEPENDENCUIES ="required-after:reborncore;after:refinedstorage";
+    public static final String COMMON_PROXY_LOC = "RebornStorage.proxys.CommonProxy";
     public static final String CLIENT_PROXY_LOC = "RebornStorage.proxys.ClientProxy";
 }
