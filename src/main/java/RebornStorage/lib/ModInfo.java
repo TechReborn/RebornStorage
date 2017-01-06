cf263c13cce5a467084aa181ac11018e558d1a74
gigabit101 <thegigabit101@gmail.com>
2017-01-06 17:30:27 +0000

    removed version

--- src/main/java/RebornStorage/lib/ModInfo.java
+++ src/main/java/RebornStorage/lib/ModInfo.java
@@ -9,6 +9,6 @@ public class ModInfo
     public static final String MOD_NAME = "RebornStorage";
     public static final String MOD_VERSION = "1.0.0";
     public static final String MOD_DEPENDENCUIES ="required-after:reborncore";
-    public static final String COMMON_PROXY_LOC = "required-after:forge;required-after:reborncore@[2,12,11);after:refinedstorage";
+    public static final String COMMON_PROXY_LOC = "required-after:forge;required-after:reborncore;after:refinedstorage";
     public static final String CLIENT_PROXY_LOC = "RebornStorage.proxys.ClientProxy";
 }
