cf5041ea80267649e6c7d87cc305ad3838520657
modmuss50 <modmuss50@gmail.com>
2018-06-16 12:00:21 +0100

    Make patters render output item when in multicrafter slot

--- src/main/java/me/modmuss50/rebornstorage/proxys/ClientProxy.java
+++ src/main/java/me/modmuss50/rebornstorage/proxys/ClientProxy.java
@@ -1,6 +1,10 @@
 package me.modmuss50.rebornstorage.proxys;
 
+import com.raoulvdberge.refinedstorage.apiimpl.API;
+import me.modmuss50.rebornstorage.client.gui.ContainerMultiCrafter;
 import me.modmuss50.rebornstorage.init.ModelHelper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.inventory.Container;
 
 /**
  * Created by Gigabit101 on 03/01/2017.
@@ -9,5 +13,18 @@ public class ClientProxy extends CommonProxy {
 	@Override
 	public void registerRenders() {
 		ModelHelper.init();
+
+		API.instance().addPatternRenderHandler(itemStack -> {
+			Container container = Minecraft.getMinecraft().player.openContainer;
+			if(container instanceof ContainerMultiCrafter){
+				for (int i = 0; i < 78; i++) {
+					if(container.getSlot(i).getStack() == itemStack){
+						return true;
+					}
+				}
+
+			}
+			return false;
+		});
 	}
 }
