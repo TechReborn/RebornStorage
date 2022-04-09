c6f245c7bf4a1d5c16a342feddcb6d1b85cf866b
Gigabit101 <thegigabit101@gmail.com>
2022-04-09 21:55:25 +0100

    Remove old StoragePart class

--- src/main/java/net/gigabit101/rebornstorage/init/ModItems.java
+++ src/main/java/net/gigabit101/rebornstorage/init/ModItems.java
@@ -3,7 +3,6 @@ package net.gigabit101.rebornstorage.init;
 import com.refinedmods.refinedstorage.api.storage.StorageType;
 import net.gigabit101.rebornstorage.client.CreativeTabRebornStorage;
 import net.gigabit101.rebornstorage.items.ItemRebornStorageCell;
-import net.gigabit101.rebornstorage.items.ItemStoragePart;
 import net.gigabit101.rebornstorage.Constants;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -14,6 +13,8 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final Item.Properties ITEM_GROUP = new Item.Properties().tab(CreativeTabRebornStorage.INSTANCE);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Constants.MOD_ID);
+    public static final Item.Properties PROPERTIES = new Item.Properties().tab(CreativeTabRebornStorage.INSTANCE);
+
 
     //ItemBlocks
     public static final RegistryObject<Item> BLOCK_MULTI_FRAME_ITEM = ITEMS.register("multiblock_frame", () -> new BlockItem(ModBlocks.BLOCK_MULTI_FRAME.get(), ITEM_GROUP));
@@ -31,13 +32,13 @@ public class ModItems {
     public static final RegistryObject<Item> STORAGE_DISK_FLUID_4096K = ITEMS.register("large_fluid_disk", () -> new ItemRebornStorageCell(4096000, StorageType.FLUID));
     public static final RegistryObject<Item> STORAGE_DISK_FLUID_16384K = ITEMS.register("larger_fluid_disk", () -> new ItemRebornStorageCell(16384000, StorageType.FLUID));
 
-    public static final RegistryObject<Item> STORAGE_DISK_256k_PART = ITEMS.register("small_item_disk_part", ItemStoragePart::new);
-    public static final RegistryObject<Item> STORAGE_DISK_1024k_PART = ITEMS.register("medium_item_disk_part", ItemStoragePart::new);
-    public static final RegistryObject<Item> STORAGE_DISK_4096K_PART = ITEMS.register("large_item_disk_part", ItemStoragePart::new);
-    public static final RegistryObject<Item> STORAGE_DISK_16384K_PART = ITEMS.register("larger_item_disk_part", ItemStoragePart::new);
+    public static final RegistryObject<Item> STORAGE_DISK_256k_PART = ITEMS.register("small_item_disk_part", () -> new Item(PROPERTIES));
+    public static final RegistryObject<Item> STORAGE_DISK_1024k_PART = ITEMS.register("medium_item_disk_part", () -> new Item(PROPERTIES));
+    public static final RegistryObject<Item> STORAGE_DISK_4096K_PART = ITEMS.register("large_item_disk_part", () -> new Item(PROPERTIES));
+    public static final RegistryObject<Item> STORAGE_DISK_16384K_PART = ITEMS.register("larger_item_disk_part", () -> new Item(PROPERTIES));
 
-    public static final RegistryObject<Item> STORAGE_DISK_256k_FLUID_PART = ITEMS.register("small_fluid_disk_part", ItemStoragePart::new);
-    public static final RegistryObject<Item> STORAGE_DISK_1024k_FLUID_PART = ITEMS.register("medium_fluid_disk_part", ItemStoragePart::new);
-    public static final RegistryObject<Item> STORAGE_DISK_4096K_FLUID_PART = ITEMS.register("large_fluid_disk_part", ItemStoragePart::new);
-    public static final RegistryObject<Item> STORAGE_DISK_16384K_FLUID_PART = ITEMS.register("larger_fluid_disk_part", ItemStoragePart::new);
+    public static final RegistryObject<Item> STORAGE_DISK_256k_FLUID_PART = ITEMS.register("small_fluid_disk_part", () -> new Item(PROPERTIES));
+    public static final RegistryObject<Item> STORAGE_DISK_1024k_FLUID_PART = ITEMS.register("medium_fluid_disk_part", () -> new Item(PROPERTIES));
+    public static final RegistryObject<Item> STORAGE_DISK_4096K_FLUID_PART = ITEMS.register("large_fluid_disk_part", () -> new Item(PROPERTIES));
+    public static final RegistryObject<Item> STORAGE_DISK_16384K_FLUID_PART = ITEMS.register("larger_fluid_disk_part", () -> new Item(PROPERTIES));
 }
