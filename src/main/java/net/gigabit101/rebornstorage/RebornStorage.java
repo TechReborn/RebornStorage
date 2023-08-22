3c6c377387d52dffa6ad75d8473961ae9f877c63
Gigabit101 <travis_n@creeperhost.net>
2023-08-22 19:11:50 +0100

    Move to datapack curio support

--- src/main/java/net/gigabit101/rebornstorage/RebornStorage.java
+++ src/main/java/net/gigabit101/rebornstorage/RebornStorage.java
@@ -79,8 +79,6 @@ public class RebornStorage
         eventBus.addListener(this::preInit);
         eventBus.addListener(this::clientInit);
         eventBus.addListener(this::registerCreativeTab);
-        InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.CHARM.getMessageBuilder().size(4).build());
-        InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("grid").icon(new ResourceLocation(Constants.MOD_ID, "items/grid")).size(1).build());
         MinecraftForge.EVENT_BUS.register(new MultiblockEventHandler());
         MinecraftForge.EVENT_BUS.register(new MultiblockServerTickHandler());
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
